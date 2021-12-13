d8bbeca81afac48b318cf0dd5ab081e55815f773
hayaa1234 <hayalawansah@gmail.com>
2021-12-13 15:53:04 +0200

    finished lab-32

--- pets/cats/views.py
+++ pets/cats/views.py
@@ -4,12 +4,16 @@ from django.shortcuts import render
 from rest_framework import generics, serializers
 from .serializers import CatSerializer
 from .models import Cat
+from .permissions import IsAuthenticatedOrReadOnly
+
 # Create your views here.
 
 class CatList(generics.ListCreateAPIView):
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
+    permission_class =(IsAuthenticatedOrReadOnly,)
 
 class Catdetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
+    permission_class =(IsAuthenticatedOrReadOnly,)
